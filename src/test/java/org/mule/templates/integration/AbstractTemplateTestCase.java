2727fedded450ec8a03ef50916d30106e6e24cae
Marek <marecica2@gmail.com>
2014-09-10 15:53:44 +0200

    initial commit

---
+++ src/test/java/org/mule/templates/integration/AbstractTemplateTestCase.java
@@ -0,0 +1,187 @@
+/**
+ * Mule Anypoint Template
+ * Copyright (c) MuleSoft, Inc.
+ * All rights reserved.  http://www.mulesoft.com
+ */
+
+package org.mule.templates.integration;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Rule;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.config.MuleProperties;
+import org.mule.construct.Flow;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.transport.NullPayload;
+
+/**
+ * This is the base test class for Templates integration tests.
+ */
+public abstract class AbstractTemplateTestCase extends FunctionalTestCase {
+
+	private static final String MAPPINGS_FOLDER_PATH = "./mappings";
+	private static final String TEST_FLOWS_FOLDER_PATH = "./src/test/resources/flows/";
+	private static final String MULE_DEPLOY_PROPERTIES_PATH = "./src/main/app/mule-deploy.properties";
+
+	protected static final int TIMEOUT_SEC = 120;
+	protected static final String TEMPLATE_NAME = "opportunity-broadcast";
+
+	protected Flow retrieveSalesOrderFromSapFlow;
+	protected Flow retrieveAccountFromSapFlow;
+
+	@Rule
+	public DynamicPort port = new DynamicPort("http.port");
+
+	@Override
+	protected String getConfigResources() {
+		String resources = "";
+		try {
+			Properties props = new Properties();
+			props.load(new FileInputStream(MULE_DEPLOY_PROPERTIES_PATH));
+			resources = props.getProperty("config.resources");
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"Could not find mule-deploy.properties file on classpath. Please add any of those files or override the getConfigResources() method to provide the resources by your own.");
+		}
+
+		return resources + getTestFlows();
+	}
+
+	protected String getTestFlows() {
+		File[] listOfFiles = new File(TEST_FLOWS_FOLDER_PATH).listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File f) {
+				return f.isFile() && f.getName().endsWith("xml");
+			}
+		});
+		
+		if (listOfFiles == null) {
+			return "";
+		}
+		
+		StringBuilder resources = new StringBuilder();
+		for (File f : listOfFiles) {
+			resources.append(",").append(TEST_FLOWS_FOLDER_PATH).append(f.getName());
+		}
+		return resources.toString();
+	}
+
+	@Override
+	protected Properties getStartUpProperties() {
+		Properties properties = new Properties(super.getStartUpProperties());
+
+		String pathToResource = MAPPINGS_FOLDER_PATH;
+		File graphFile = new File(pathToResource);
+
+		properties.put(MuleProperties.APP_HOME_DIRECTORY_PROPERTY, graphFile.getAbsolutePath());
+
+		return properties;
+	}
+
+	protected void deleteTestOpportunityFromSandBox(List<Map<String, Object>> createdOpportunities) throws Exception {
+		List<String> idList = new ArrayList<String>();
+
+		// Delete the created opportunities in Salesforce
+		Flow flow = lookupFlowConstruct("deleteOpportunityFromSalesforceFlow");
+//		flow.initialise();
+		for (Map<String, Object> c : createdOpportunities) {
+			idList.add((String) c.get("Id"));
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+		idList.clear();
+
+		// Delete the created opportunities in SAP
+		flow = lookupFlowConstruct("deleteSalesOrderFromSapFlow");
+//		flow.initialise();
+		for (Map<String, Object> c : createdOpportunities) {
+			System.out.println("CCC: " + c);
+			Map<String, Object> opportunity = invokeRetrieveFlow(retrieveSalesOrderFromSapFlow, c);
+			if (opportunity != null) {
+				idList.add((String) opportunity.get("Id"));
+			}
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+	}
+
+	protected void deleteTestAccountFromSandBox(List<Map<String, Object>> createdAccounts) throws Exception {
+		// Delete the created accounts in Salesforce
+		Flow flow = lookupFlowConstruct("deleteAccountFromSalesforceFlow");
+//		flow.initialise();
+
+		List<Object> idList = new ArrayList<Object>();
+		for (Map<String, Object> c : createdAccounts) {
+			idList.add(c.get("Id"));
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+
+		// Delete the created accounts in SAP
+		flow = lookupFlowConstruct("deleteAccountFromSapFlow");
+//		flow.initialise();
+		idList.clear();
+		for (Map<String, Object> c : createdAccounts) {
+			Map<String, Object> account = invokeRetrieveFlow(retrieveAccountFromSapFlow, c);
+			if (account != null) {
+				System.out.println("AAAAACCCCC: " + account);
+				idList.add(account.get("Id"));
+			}
+		}
+		flow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+	}
+
+	protected String buildUniqueName(String templateName, String name) {
+		String timeStamp = new Long(new Date().getTime()).toString();
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(name);
+		builder.append(templateName);
+		builder.append(timeStamp);
+
+		return builder.toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Map<String, Object> invokeRetrieveFlow(Flow flow, Map<String, Object> payload) throws Exception {
+		MuleEvent event = flow.process(getTestEvent(payload, MessageExchangePattern.REQUEST_RESPONSE));
+
+		Object resultPayload = event.getMessage().getPayload();
+		return resultPayload instanceof NullPayload ? null : (Map<String, Object>) resultPayload;
+	}
+
+	protected Map<String, Object> createOpportunity(String orgId, int sequence) throws ParseException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("Name", buildUniqueName(TEMPLATE_NAME, "OppName" + sequence + "_"));
+		map.put("StageName", "NoStage");
+		map.put("CloseDate", date("2050-10-10"));
+		map.put("Probability", "1");
+		return map;
+	}
+
+	private Date date(String dateString) throws ParseException {
+		return new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+	}
+
+	protected String buildUniqueEmail(String user) {
+		String server = "fakemail";
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(buildUniqueName(TEMPLATE_NAME, user));
+		builder.append("@");
+		builder.append(server);
+		builder.append(".com");
+
+		return builder.toString();
+	}
+}
---
+++ src/test/java/org/mule/templates/integration/BusinessLogicTestCreateAccountIT.java
@@ -0,0 +1,169 @@
+/**
+ * Mule Anypoint Template
+ * Copyright (c) MuleSoft, Inc.
+ * All rights reserved.  http://www.mulesoft.com
+ */
+
+package org.mule.templates.integration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.construct.Flow;
+import org.mule.context.notification.NotificationException;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Prober;
+import org.mule.templates.test.utils.ListenerProbe;
+import org.mule.templates.test.utils.PipelineSynchronizeListener;
+
+import com.mulesoft.module.batch.BatchTestHelper;
+import com.sforce.soap.partner.SaveResult;
+
+/**
+ * The objective of this class is to validate the correct behavior of the Mule Template that make calls to external systems.
+ * 
+ * The test will invoke the batch process and afterwards check that the opportunities had been correctly created and that the ones that should be filtered are
+ * not in the destination sand box.
+ * 
+ * The test validates that an account will get sync as result of the integration.
+ */
+public class BusinessLogicTestCreateAccountIT extends AbstractTemplateTestCase {
+
+	private static final String POLL_FLOW_NAME = "triggerFlow";
+	private BatchTestHelper helper;
+	private List<Map<String, Object>> createdOpportunities = new ArrayList<Map<String, Object>>();
+	private List<Map<String, Object>> createdAccounts = new ArrayList<Map<String, Object>>();
+
+	@BeforeClass
+	public static void init() {
+
+		System.setProperty("page.size", "1000");
+
+		// Set the frequency between polls to 10 seconds
+		System.setProperty("poll.frequencyMillis", "10000");
+
+		// Set the poll starting delay to 20 seconds
+		System.setProperty("poll.startDelayMillis", "20000");
+
+		// Setting Default Watermark Expression to query SFDC with
+		// LastModifiedDate greater than ten seconds before current time
+		System.setProperty("watermark.default.expression", "#[groovy: new Date(System.currentTimeMillis() - 10000).format(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\", TimeZone.getTimeZone('UTC'))]");
+
+		System.setProperty("account.sync.policy", "syncAccount");
+		System.setProperty("account.id.in.b", "");
+
+	}
+
+	@AfterClass
+	public static void shutDown() {
+		System.clearProperty("account.sync.policy");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		helper = new BatchTestHelper(muleContext);
+		retrieveSalesOrderFromSapFlow = lookupFlowConstruct("retrieveSalesOrderFromSapFlow");
+		retrieveAccountFromSapFlow = lookupFlowConstruct("retrieveAccountFromSapFlow");
+		createTestDataInSandBox();
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+		deleteTestDataFromSandBox();
+	}
+
+	@Test
+	public void testMainFlow() throws Exception {
+
+		// Run poll and wait for it to run
+		runFlow(POLL_FLOW_NAME);
+
+		// Wait for the batch job executed by the poll flow to finish
+		helper.awaitJobTermination(TIMEOUT_SEC * 1000, 500);
+		helper.assertJobWasSuccessful();
+
+		Assert.assertEquals("The opportunity should not have been sync", null, invokeRetrieveFlow(retrieveSalesOrderFromSapFlow, createdOpportunities.get(0)));
+
+		Assert.assertEquals("The opportunity should not have been sync", null, invokeRetrieveFlow(retrieveSalesOrderFromSapFlow, createdOpportunities.get(1)));
+
+		Map<String, Object> accountPayload = invokeRetrieveFlow(retrieveAccountFromSapFlow, createdAccounts.get(0));
+		Map<String, Object> opportunityPayload = invokeRetrieveFlow(retrieveSalesOrderFromSapFlow, createdOpportunities.get(2));
+		Assert.assertEquals("The opportunity should have been sync", createdOpportunities.get(2).get("Name"), opportunityPayload == null ? null : opportunityPayload.get("Name"));
+		Assert.assertEquals("The opportunity should belong to a different account ", accountPayload.get("CustomerNumber"), opportunityPayload == null ? null : opportunityPayload.get("AccountId"));
+	}
+
+	private void createTestDataInSandBox() throws MuleException, Exception {
+		createAccounts();
+		createOpportunities();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createAccounts() throws Exception {
+		Flow flow = lookupFlowConstruct("createAccountInSalesforceFlow");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("Name", "sfdc2sap_opp_bc_" + Long.toString(System.currentTimeMillis(), Character.MAX_RADIX));
+		map.put("BillingCity", "San Francisco");
+		map.put("BillingCountry", "USA");
+		map.put("Phone", "123456789");
+		map.put("Industry", "Education");
+		map.put("NumberOfEmployees", 9000);
+		
+		createdAccounts.add(map);
+
+		MuleEvent event = flow.process(getTestEvent(createdAccounts, MessageExchangePattern.REQUEST_RESPONSE));
+		List<SaveResult> results = (List<SaveResult>) event.getMessage().getPayload();
+		for (int i = 0; i < results.size(); i++) {
+			createdAccounts.get(i).put("Id", results.get(i).getId());
+		}
+
+		System.out.println("Results of data creation in sandbox" + createdAccounts.toString());
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createOpportunities() throws Exception {
+		Flow flow = lookupFlowConstruct("createOpportunityInSalesforceFlow");
+
+		// This opportunity should not be sync
+		Map<String, Object> opportunity = createOpportunity("Salesforce", 0);
+		opportunity.put("Amount", 300);
+		createdOpportunities.add(opportunity);
+
+		// This opportunity should not be sync
+		opportunity = createOpportunity("Salesforce", 1);
+		opportunity.put("Amount", 1000);
+		createdOpportunities.add(opportunity);
+
+		// This opportunity should BE sync with it's account
+		opportunity = createOpportunity("Salesforce", 2);
+		opportunity.put("Amount", 30000);
+		opportunity.put("AccountId", createdAccounts.get(0).get("Id"));
+		opportunity.put("StageName", "Closed Won");
+		createdOpportunities.add(opportunity);
+
+		MuleEvent event = flow.process(getTestEvent(createdOpportunities, MessageExchangePattern.REQUEST_RESPONSE));
+		List<SaveResult> results = (List<SaveResult>) event.getMessage().getPayload();
+		for (int i = 0; i < results.size(); i++) {
+			createdOpportunities.get(i).put("Id", results.get(i).getId());
+		}
+	}
+
+	private void deleteTestDataFromSandBox() throws MuleException, Exception {
+		deleteTestOpportunityFromSandBox(createdOpportunities);
+		deleteTestAccountFromSandBox(createdAccounts);
+	}
+
+}
